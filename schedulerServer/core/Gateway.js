d789b170fd39a06fccfea427d5cb990689aabaff
JerrodSunderland <jerrod.sunderland@gmail.com>
2021-01-03 20:57:14 -0500

    comment: removed authorize for now

--- schedulerServer/core/Gateway.js
+++ schedulerServer/core/Gateway.js
@@ -3,8 +3,33 @@ const config = require('../config/config');
 const utility = require("./Utility")
 const auth = require("./Authentication")
 
-// get all
+// verify token
 module.exports.verifyAccess = async (event, context, callback) => {
-    var token = event.access_token;
     console.log(event)
+    context.succeed(generatePolicy('user', 'Allow', event.methodArn));
+    callback(null, generatePolicy('user', 'Allow', event.methodArn));
+//     var token = event.authorizationToken;
+//     if (token.startsWith("Bearer ")){
+//         token = token.substring(7, token.length);
+//    } else {
+//         // error
+//    }
+//     var loginInfo = await auth.verifyJWTToken(token)
+//     console.log(loginInfo)
+}
+
+// Help function to generate an IAM policy
+var generatePolicy = function(principalId, effect, resource) {
+    var authResponse = {};
+
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        var policyDocument = {};
+        policyDocument.Version = '2021-01-03';
+        policyDocument.Statement = [];
+        var statementOne = {};
+        statementOne.Action = 'execute-api:Invoke';
+        statementOne.Effect = effect;
+    }
+    return authResponse
 }
